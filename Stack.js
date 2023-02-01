57b6260d62498b31cfedbb624d4bcc1379c4a35b
peter3khalil <rikokamil97@gmail.com>
2023-02-01 20:54:33 +0200

    Implementation of Stack with unit testing

---
+++ Stack.js
@@ -0,0 +1,21 @@
+class Stack{
+    constructor(){
+        this.items = [];
+    }
+    push(element){
+        this.items.push(element); // O(1)
+    }
+    pop(){
+        return this.isEmpty()? -1: this.items.pop(); // O(1)
+    }
+    peek(){
+        return this.isEmpty() ? -1 : this.items[this.size()-1]; // O(1)
+    }
+    isEmpty(){
+        return this.size() ===0; // O(1)
+    }
+    size(){
+        return this.items.length; // O(1)
+    }
+}
+module.exports = Stack;
---
+++ stack.test.js
@@ -0,0 +1,46 @@
+const Stack = require("./Stack")
+let stack = new Stack();
+
+describe('Test Stack methods in case : Stack is empty', () => {
+    it('isEmpty should return true', () => {
+        expect(stack.isEmpty()).toBeTruthy();
+    });
+    it('pop() it should return -1', () => {
+        expect(stack.pop()).toBe(-1)
+    });
+    it('peak() should return -1', () => {
+        expect(stack.peek()).toBe(-1);
+    });
+    it('size() should return 0', () => {
+        expect(stack.size()).toBe(0);
+    });
+    
+});
+
+describe('Test Stack methods in case : Stack is not empty', () => {
+    it('push() should insert element in last', () => {
+        stack.push(10);
+        expect(stack.peek()).toBe(10);
+        stack.push(20);
+        expect(stack.peek()).toBe(20);
+        stack.push(-1);
+        expect(stack.peek()).toBe(-1);
+        stack.push(80);
+        expect(stack.peek()).toBe(80);
+    });
+    it('size() should not be Zero', () => {
+        expect(stack.size()).not.toBe(0)
+    });
+    
+    it('isEmpty should return false', () => {
+        expect(stack.isEmpty()).toBeFalsy();
+    });
+    it('pop() should return 80', () => {
+        expect(stack.pop()).toBe(80)
+    });
+    
+});
+
+
+
+
